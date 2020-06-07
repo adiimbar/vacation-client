ad9388e12146902e2d5470ab347451762495bf06
adiimbar <adiimbar@users.noreply.github.com>
2020-06-08 01:09:04 +0300

    header error

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import { Menu } from 'antd';
+import { Menu } from 'antd';
 import "./header.css";
 
 
@@ -12,11 +12,11 @@ export default class Header extends Component{
         return (
             <div className="header">
                 <h1>Header works!</h1>
-                {/* <Menu theme="dark" mode="horizontal">
+                <Menu theme="dark" mode="horizontal">
                     <Menu.Item key="1">nav 1</Menu.Item>
                     <Menu.Item key="2">nav 2</Menu.Item>
                     <Menu.Item key="3">nav 3</Menu.Item>
-                </Menu> */}
+                </Menu>
             </div>
         );
     }
