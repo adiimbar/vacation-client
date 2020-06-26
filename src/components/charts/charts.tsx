79ddd092be844b3532db0fd7eb8a2b2ee9b8dbe0
adiimbar <adiimbar@users.noreply.github.com>
2020-06-26 17:52:43 +0300

    :sparkles: charts works

--- src/components/charts/charts.tsx
+++ src/components/charts/charts.tsx
@@ -9,12 +9,13 @@ import apiService from '../../services/api.service';
 
 import { Button } from 'antd';
 import {Bar} from 'react-chartjs-2';
+// import Chart from 'chart.js';
 
 
 interface ToursFollowersState {
     toursFollowers: ToursFollowersDetails[],
-    vacationsDestinations: any,
-    vacationsFollowersNumbers: any
+    // vacationsDestinations: any,
+    // vacationsFollowersNumbers: any
 }
 
 const graphOptions: any = {
@@ -28,8 +29,6 @@ const graphOptions: any = {
     }
 }
 
-// let myChart:any
-
 export default class Charts extends Component <any, ToursFollowersState>{
 
     private unsubscribeStore: Unsubscribe;
@@ -38,31 +37,13 @@ export default class Charts extends Component <any, ToursFollowersState>{
         super(props);
         this.state = {
             toursFollowers:[],
-            vacationsDestinations:[],
-            vacationsFollowersNumbers:[] 
+            // vacationsDestinations:[],
+            // vacationsFollowersNumbers:[] 
         
         };
 
         this.backToAdminHandler = this.backToAdminHandler.bind(this)
-        // let tourDestinations: any = [];
     }
-
-    componentDidUpdate(prevProps: any, prevState: any) {
-        // Typical usage (don't forget to compare props):
-        if (this.state.toursFollowers !== prevState.toursFollowers) {
-            this.state.toursFollowers.forEach(element => {
-                // this.setState.tourDestinations.push(element.destination)
-                this.setState({vacationsDestinations: this.state.vacationsDestinations.push(element.destination)})
-                // console.log('inside foreach loop')
-                // console.log(this.state.vacationsDestinations)
-            });
-        }
-        console.log(prevProps);
-        console.log(prevState);
-        let asdf = store.getState().userType;
-        console.log(asdf);
-
-      }
 
     componentWillUnmount(){
         this.unsubscribeStore();
@@ -78,93 +59,49 @@ export default class Charts extends Component <any, ToursFollowersState>{
             })
         );
 
-
-
-        // const myChart = new Chart(this.chartRef.current, {
-        //     type: 'line',
-        //     options: {
-        //       scales: {
-        //         xAxes: [
-        //           {
-        //             type: 'time',
-        //             time: {
-        //               unit: 'week'
-        //             }
-        //           }
-        //         ],
-        //         yAxes: [
-        //           {
-        //             ticks: {
-        //               min: 0
-        //             }
-        //           }
-        //         ]
-        //       }
-        //     },
-        //     data: {
-        //       labels: this.props.data.map(d => d.time),
-        //       datasets: [{
-        //         label: this.props.title,
-        //         data: this.props.data.map(d => d.value),
-        //         fill: 'none',
-        //         backgroundColor: this.props.color,
-        //         pointRadius: 2,
-        //         borderColor: this.props.color,
-        //         borderWidth: 1,
-        //         lineTension: 0
-        //       }]
-        //     }
-        //   });
-
     }
 
     getNumberOfFollowersForAllTours = async () => {
         const response = await apiService.get<ToursFollowersDetails[]>("follow");
         store.dispatch({ type: ActionType.getNumberOfFollowersForAllTours, payload: response.data});
-        // this.setState({ vacations: response.data });
-        console.log(this.state.toursFollowers);
-        // console.log(response.data);
     }
 
+    getChartData() {
+        const vacationFollowers: any = this.state.toursFollowers;
 
+        const labels: string[] = [];
+        const values: number[] = [];
 
-    // addData = (chart:any, label:any, data:any) => {
-    //     chart.data.labels.push(label);
-    //     chart.data.datasets.forEach((dataset:any) => {
-    //         dataset.data.push(data);
-    //     });
-    //     chart.update();
-    // }
-    
-    // removeData = (chart:any) => {
-    //     chart.data.labels.pop();
-    //     chart.data.datasets.forEach((dataset:any) => {
-    //         dataset.data.pop();
-    //     });
-    //     chart.update();
-    // }
+        if(vacationFollowers) {
+            vacationFollowers.forEach((statFollowerObj: any) => {
+                const { destination, numOfFollowers} = statFollowerObj;
+                labels.push(destination);
+                values.push(numOfFollowers);
+            });
+        }
+
+        return {
+            labels,
+            datasets: [
+                {
+                    label: 'Amount of followers',
+                    backgroundColor: 'rgba(75,192,192,0.4)',
+                    borderColor: 'rgba(75,192,192,1)',
+                    borderWidth: 1,
+                    hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                    hoverBorderColor: 'rgba(255,99,132,1)',
+                    data: values
+                }        
+            ]
+        }
+
+    }
+
 
     backToAdminHandler() {
         this.props.history.push('/admin');
     }
     
-    data = {
-        // labels: ['aaa', 'bbb'],
-        labels: ['aaa'],
-        datasets: [
-          {
-            label: 'Amount of followers',
-            backgroundColor: 'rgba(75,192,192,0.4)',
-            borderColor: 'rgba(75,192,192,1)',
-            borderWidth: 1,
-            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-            hoverBorderColor: 'rgba(255,99,132,1)',
-            data: [20]
-            // data: this.state.vacationsFollowersNumbers
-          }
-        ]
-    };
-
 
     public render() {
         return (
@@ -177,7 +114,7 @@ export default class Charts extends Component <any, ToursFollowersState>{
                 <div>
                     <h2>Vacations followers statistics</h2>
                     <Bar
-                    data={this.data}
+                    data={this.getChartData()}
                     // width={100}
                     height={350}
                     options={
