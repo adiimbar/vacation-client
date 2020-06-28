34f052bf373db76a0232d95f50be16ef0a0da988
adiimbar <adiimbar@users.noreply.github.com>
2020-06-28 17:46:10 +0300

    added logo and commiting background image

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,36 @@
 import React, {  } from 'react';
 import "./header.css";
 
+// import { Unsubscribe } from "redux";
+// import { store } from '../../redux/store';
+// import { ActionType } from '../../redux/action-type';
+
+// import AddVacationComponent from '../addVacationComponent/addVacationComponent';
+// import { Button } from 'antd';
+
 
 const Header = (props: any) => {
 
+    // const userType = store.getState().userType;
+
+    // function watchChartsHandler() {
+    //     console.log(props);
+    //     // props.history.push('/charts');
+    // }
+
+    const url: string = "http://localhost:3001/uploads/Logo.png";
+    
     return (
         <div className="headerClass">
-            <h1>Vacation Addict</h1>
+            <img src={url} className="logoClass" alt="logo"></img>
+                {/* {
+                    userType === 'ADMIN' ? 
+                        <div className="adminButtonsClass">
+                        <AddVacationComponent />
+                        <Button type="primary" className="statisticsButton" onClick={watchChartsHandler}>watch statistics</Button>
+                        </div> : 
+                            <> </>
+                } */}
         </div>
     )
 
